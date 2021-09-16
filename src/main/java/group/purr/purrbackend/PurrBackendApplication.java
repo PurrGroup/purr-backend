2f2a0de9346422fe78ca66d64dcca16c56eb5c45
QiChen <1021705534@qq.com>
2021-09-16 22:05:41 +0800

    first blood

---
+++ src/main/java/group/purr/purrbackend/PurrBackendApplication.java
@@ -0,0 +1,13 @@
+package group.purr.purrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurrBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/purr/purrbackend/PurrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package group.purr.purrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
