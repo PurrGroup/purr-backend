7c43720057a731143f9a8e9e5502a9129fbe9cfa
Hwa <hwahe.cs@gmail.com>
2021-09-27 20:31:37 +0800

    fix: add @EqualsAndHashCode to some DO

--- src/main/java/group/purr/purrbackend/entity/Author.java
+++ src/main/java/group/purr/purrbackend/entity/Author.java
@@ -1,9 +1,6 @@
 package group.purr.purrbackend.entity;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
@@ -20,6 +17,7 @@ import javax.persistence.Id;
 @DynamicInsert
 @DynamicUpdate
 @Entity
+@EqualsAndHashCode
 public class Author {
     @Id
     private String optionKey;
