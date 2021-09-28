bd21b23ab03787f54f03f6d14be977f2a339b25e
QiChen <1021705534@qq.com>
2021-09-28 10:22:35 +0800

    feat:修改service层架构

--- src/main/java/group/purr/purrbackend/service/MetaService.java
+++ src/main/java/group/purr/purrbackend/service/MetaService.java
@@ -5,19 +5,8 @@ import group.purr.purrbackend.repository.BlogmetaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MetaService {
+public interface MetaService {
+    boolean isInstalled();
 
-    @Autowired
-    BlogmetaRepository blogmetaRepository;
-
-    /**
-     * 判断是否已安装博客
-     * @return 返回一个布尔值，若为true则已安装
-     */
-    public boolean isInstalled() {
-        Long count = blogmetaRepository.countAllByOptionName("blog_title");
-        return count != 0;
-    }
 }
 
