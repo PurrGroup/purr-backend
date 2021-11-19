6ff3b7230ff25ae2987f4d10bff027c93a1c2f80
Hwa <hwahe.cs@gmail.com>
2021-11-19 11:58:32 +0800

    feat: add root path mapping for tomcat

--- src/main/java/group/purr/purrbackend/config/PurrMvcConfiguration.java
+++ src/main/java/group/purr/purrbackend/config/PurrMvcConfiguration.java
@@ -5,12 +5,16 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.File;
 import java.nio.file.FileSystems;
 
 @Configuration
 public class PurrMvcConfiguration implements WebMvcConfigurer {
 
     private final Environment env;
+    private static final String FILE_PROTOCOL = "file:///";
+    private static final String WORK_DIR = "~/.purr";
+    private static final String FILE_SEPARATOR = File.separator;
 
     public PurrMvcConfiguration(Environment env) {
         this.env = env;
@@ -21,16 +25,18 @@ public class PurrMvcConfiguration implements WebMvcConfigurer {
 
         String mediaPath = env.getProperty("purr.media.path");
         assert mediaPath != null;
-        String workDir = FileSystems.getDefault().getPath(mediaPath).normalize().toAbsolutePath() + "/";
+        String mediaDir = FileSystems.getDefault().getPath(mediaPath).normalize().toAbsolutePath() + FILE_SEPARATOR;
+        String workDir = FileSystems.getDefault().getPath(WORK_DIR).normalize().toAbsolutePath() + FILE_SEPARATOR;
 
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/")
                 .addResourceLocations("classpath:/resources/")
                 .addResourceLocations("classpath:/static/")
-                .addResourceLocations("classpath:/public/");
+                .addResourceLocations("classpath:/public/")
+                .addResourceLocations(FILE_PROTOCOL + workDir + "static");
 
         registry.addResourceHandler("/media/**")
-                .addResourceLocations("file://" + workDir);
+                .addResourceLocations(FILE_PROTOCOL + mediaDir);
 
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
